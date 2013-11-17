dc6b32513a4ef4c309e67d2fb7c1175753bcf3dc
David Rowe <david@ctrlaltstudio.com>
2013-11-17 13:10:19 -0800

    Fix joystick to use correct walk speed.

--- indra/newview/llappviewer.cpp
+++ indra/newview/llappviewer.cpp
@@ -1722,10 +1722,10 @@ bool LLAppViewer::mainLoop()
 					&& !gViewerWindow->getShowProgress()
 					&& !gFocusMgr.focusLocked())
 				{
-					joystick->scanJoystick();
 					// <CV:David>
 					gAgent.updateWalkSpeed();
 					// </CV:David>
+					joystick->scanJoystick();
 					gKeyboard->scanKeyboard();
 					// <FS:Ansariel> Chalice Yao's crouch toggle
 					static LLCachedControl<bool> fsCrouchToggle(gSavedSettings, "FSCrouchToggle");
